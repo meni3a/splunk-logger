93f60acecfd6ac55f51ac735c09f1929b30df22f
meni3a <meni3a@gmail.com>
2021-01-18 00:44:22 +0200

    fix setup

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import request = require('request');
 export class SplunkLogger{
 
     constructor(url:String,token:String) {
-        this.options.headers.Authorization = `Splunk ${token}`;
-        this.options.url = url.toString();
+        this.options.headers.Authorization = `Splunk ${token}`
+        this.options.url = url.toString()
+        
+        this.initial()
     }
+
+    private writeLog:boolean = true
 
     private options = {
         'method': 'POST',
@@ -18,22 +22,34 @@ export class SplunkLogger{
       
       };
 
+      private isFirstLog:boolean = true
+      public isLogsPrinted:boolean = true
+
       private send(type:string, message:string){
-        var payload = {
+        let payload = {
             event: {
                 message: message,
                 type: type
             }
         }
-        this.options.body = JSON.stringify(payload);
+        this.options.body = JSON.stringify(payload)
+        
+        const self = this
         request(this.options, function (error, response) {
             if (error){
-                console.log("SplunkLogger Error: ",error);
+                console.log("SplunkLogger Error: ",error)
             }
             else{
                 const result = JSON.parse(response?.body)
                 if(result?.code == 0){
-                    console.log(result?.text)
+                    if(self.isFirstLog){
+                        console.log("Logger is initial successfully")
+                        self.isFirstLog = false
+                    }
+                    else{
+                        if(self.isLogsPrinted)
+                        console.log(`${new Date().toISOString().substr(11, 8)} | ${type} | ${message}`)
+                    }
                 }
                 else if(result?.code != 0){
                     console.log("SplunkLogger Error: " + result?.text)
@@ -44,8 +60,10 @@ export class SplunkLogger{
             }
 
         });
+
       
     }
+    private initial(){this.send("initial","Logger initialed")}
 
     public error(message:string){ this.send("error",message)}
 
