ef51194bd80a4961dd90be1bd0309d17ea6a443f
meni3a <meni3a@gmail.com>
2021-01-30 20:07:52 +0200

    add singleton pattern

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
-const request = require('request')
+const fetch = require('node-fetch');
 
 
+class httpRequest {
+    constructor(url:string,method:string,headers:object,body:any){
+        this.url = url
+        this.method = method
+        this.headers = headers
+        this.body = body
+    }
+    url: string
+    method: string
+    headers: object
+    body: any
+
+}
+class splunkMessage{
+    constructor(type?: string,message?:any){
+        this.type = type 
+        this.message = message
+    }
+    type: string | undefined;
+    message:any
+}
+class splunkPayload{
+    constructor(type?: string,message?:any){
+        this.event = new splunkMessage(type, message)
+    }
+    
+    event:splunkMessage
+}
 
 export class SplunkLogger{
 
     constructor(url:String,token:String) {
-        this.options.headers.Authorization = `Splunk ${token}`
-        this.options.url = url.toString()
-        
+        this.queue = [];
+        this.processing = false;
+        this.fn = fetch;
+
+        this.url = url
+        this.token = token
         this.initial()
     }
+
+    private processing: boolean;
+    private queue: httpRequest[];
+    private fn: Function;
+
+    private url: String;
+    private token: String;
 
     private colors = {
         regular: "\u001b[1;0m",
@@ -20,84 +58,86 @@ export class SplunkLogger{
         
     }
 
-    private options = {
-        'method': 'POST',
-        'url': '',
-        'headers': {
-          'Authorization': '',
-        },
-        body: ''
-      
-      }
 
-      private isFirstLog:boolean = true
-      public isLogsPrinted:boolean = true
+    public isLogsPrinted:boolean = true
 
-      private send(type:string, message:any){
-        let payload = {
-            event: {
-                message: message,
-                type: type
-            }
-        }
-        this.options.body = JSON.stringify(payload)
+    async run() {
         
-        const self = this
-        request(this.options, function (error:Error, response:any) {
-            if (error){
-                console.log("SplunkLogger Error: ",error)
-            }
-            else{
-                const result = JSON.parse(response?.body)
+        while (!this.processing && this.queue.length) {
+            try {
+                this.processing = true;
+                let request = this.queue.shift()  
+
+                const payload = <splunkPayload>(request?.body);
+                if(request!=null){   
+                    request.body =JSON.stringify(payload)
+
+                }
+
+                const response = await this.fn(request?.url,request)       
+                const result = await response.json();
+
                 if(result?.code == 0){
-                    if(self.isFirstLog){
-                        console.log("Logger initialed successfully")
-                        self.isFirstLog = false
-                    }
-                    else{
 
-                        if(self.isLogsPrinted){
-                            let color = self.colors.regular
-                            switch(type){
-                                case "ERROR":
-                                    color = self.colors.red
-                                    break
-                                case "INFO":
-                                    color = self.colors.green
-                                    break
-                                case "WARN":
-                                    color = self.colors.yellow
-                                    break
-                                case "FATAL":
-                                    color = self.colors.red
-                                    break
-                                case "DEBUG":
-                                    color = self.colors.cyan
-                                    break 
-                                default:
-                                    break;                                
-                                
-                            }
-
-                            const log = `${new Date().toISOString().substr(11, 8)} - ${color} ${type} ${self.colors.regular} - ${JSON.stringify(message)}`
-                            console.log(log)
-                       
+                    if(this.isLogsPrinted){
+                        let color = this.colors.regular
+                        switch(payload.event.type){
+                            case "ERROR":
+                                color = this.colors.red
+                                break 
+                            case "INFO":
+                                color = this.colors.green
+                                break
+                            case "WARN":
+                                color = this.colors.yellow
+                                break
+                            case "FATAL":
+                                color = this.colors.red
+                                break
+                            case "DEBUG":
+                                color = this.colors.cyan
+                                break 
+                            case "INITIAL":
+                                color = this.colors.cyan
+                                break 
+                            default:
+                                break;                                
+                            
                         }
 
+                        const log = `${new Date().toISOString().substr(11, 8)} - ${color} ${payload.event.type} ${this.colors.regular} - ${JSON.stringify(payload.event.message)}`
+                        console.log(log)
+                
                     }
-                }
-                else if(result?.code != 0){
-                    console.log("SplunkLogger Error: " + result?.text)
+
                 }
                 else{
-                    console.log("SplunkLogger: Unexpected response")
+                    console.log("SplunkLogger Error: " + result?.text)
                 }
             }
+            catch(err) {
+                console.log("SplunkLogger Fail: ",err)
+            } 
+            finally{
+                this.processing = false;
+            }
+        }
 
-        });
-
-      
     }
+     
+
+    private send(type:string, message:any){
+
+        const headers = {Authorization:`Splunk ${this.token}`}
+        const body = new splunkPayload(type,message)
+        const request = new httpRequest(this.url.toString(),"POST",headers, body)
+
+        this.queue.push(request)
+
+        this.run()
+    }
+
+
     private initial(){this.send("INITIAL","Logger initialed")}
 
     public error(message:any){ this.send("ERROR",message)}
